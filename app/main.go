fb21d6a6c9b6858961fb852aea28039b1c969799
Aleksandra Vorobeva <V-Sash@mail.ru>
2022-01-30 17:07:41 +0300

    3: html

--- app/main.go
+++ app/main.go
@@ -4,44 +4,26 @@ import (
 	"fmt"
 	"net/http"
 	"html/template"
+
 )
 
-type User struct {
-	Name  string
-	Age   uint16
-	Money     int16
-	AvgGrades float64
-	Happiness float64
-	Hobbies []string
-}
+func index(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles("template/index.html")
 
-func (u User) getAllInfo() string {
-	return fmt.Sprintf("User name is %s. He is %d years old.", u.Name, u.Age)
-}
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
 
-func (u *User) setNewName(newName string) {
-	u.Name = newName
+	temp.ExecuteTemplate(w, "index", nil)
+
 }
 
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	bob := User{"Bob", 25, 50, 4.8, 0.8, []string{"football", "dance"}}
-	//fmt.Fprintf(w, "<b>Main Text</b>")
-	tmpl, _ := template.ParseFiles("templates/home_page.html")
-	tmpl.Execute(w, bob)
-}
-
-func contactsPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Contacts")
-}
-
-func handleRequest() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/contacts/", contactsPage)
-	http.ListenAndServe(":8080", nil)
+func handleFunc() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8090", nil)
 }
 
 func main() {
-
-	handleRequest()
+	handleFunc()
 }

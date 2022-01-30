c5256f080bc907fd6c75c52484e9037bb869f040
Aleksandra Vorobeva <V-Sash@mail.ru>
2022-01-30 13:47:58 +0300

    2: html start

--- app/main.go
+++ app/main.go
@@ -3,10 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
 )
 
+type User struct {
+	Name  string
+	Age   uint16
+	Money     int16
+	AvgGrades float64
+	Happiness float64
+	Hobbies []string
+}
+
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is %s. He is %d years old.", u.Name, u.Age)
+}
+
+func (u *User) setNewName(newName string) {
+	u.Name = newName
+}
+
+
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "home")
+	bob := User{"Bob", 25, 50, 4.8, 0.8, []string{"football", "dance"}}
+	//fmt.Fprintf(w, "<b>Main Text</b>")
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
 }
 
 func contactsPage(w http.ResponseWriter, r *http.Request) {
@@ -20,5 +42,6 @@ func handleRequest() {
 }
 
 func main() {
+
 	handleRequest()
 }
